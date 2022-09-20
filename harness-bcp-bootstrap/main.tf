f137ce5b11059d64448979a3857bc0395b8a39a7
Cristian Ramirez <cristiano.rosetti@gmail.com>
2022-09-19 23:25:23 -0500

    initial commit

---
+++ harness-bcp-bootstrap/main.tf
@@ -0,0 +1,120 @@
+module "bootstrap_harness" {
+  source                    = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
+  harness_platform_projects = local.harness_platform_projects
+
+  providers = {
+    harness = harness.provisioner
+  }
+}
+
+module "bootstrap_connector" {
+  depends_on = [
+    module.bootstrap_harness,
+    # module.bootstrap_harness_delegates
+  ]
+  source                      = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
+  harness_platform_connectors = local.harness_platform_connectors
+
+  providers = {
+    harness = harness.provisioner
+  }
+}
+
+resource "harness_platform_secret_text" "harness_secrets" {
+  depends_on = [
+    module.bootstrap_harness,
+  ]
+  for_each                  = local.harness_platform_secrets
+  identifier                = lower(replace(each.key, "/[\\s-.]/", "_"))
+  name                      = each.key
+  description               = "${each.key} - ${each.value.description}"
+  secret_manager_identifier = "harnessSecretManager"
+  value_type                = "Inline"
+  value                     = each.value.secret
+  org_id                    = each.value.org_id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "local_file" "template" {
+  depends_on = [
+    module.bootstrap_connector,
+  ]
+  content = templatefile("../contrib/harness/templates/terraform-pipeline.tpl", {
+    org_identifier     = module.bootstrap_harness.organization[var.cristian_lab_org_projects.organization_name].org_id
+    git_connector_ref  = "org.bcp_lab_github_connector" # TODO: get name dynamically
+    secret_manager_ref = "org.harnessSecretManager"
+    approver_ref       = "account.SE_Admin" # TODO: get name dynamically
+    delegate_ref       = "cristian-gke-tf"  # TODO: get name dynamically
+    store_type_ref     = "Github"
+    provisioner_ref    = "<+pipeline.stages.[\"Provisioning\"].variables.[\"provisioner_ref\"]>"
+
+    tf_backend = {
+      username = "<+pipeline.stages.[\"Provisioning\"].variables.[\"tf_backend_username\"]>"
+      password = "<+pipeline.stages.[\"Provisioning\"].variables.[\"tf_backend_password\"]>"
+      url      = "<+pipeline.stages.[\"Provisioning\"].variables.[\"tf_backend_url\"]>"
+      repo     = "<+pipeline.stages.[\"Provisioning\"].variables.[\"tf_backend_repo\"]>"
+      subpath  = "<+pipeline.stages.[\"Provisioning\"].variables.[\"tf_backend_subpath\"]>"
+    }
+    tf_variables = {
+      harness_platform_api_key    = "<+pipeline.stages.[\"Provisioning\"].variables.[\"harness_platform_api_key\"]>"
+      harness_platform_account_id = "<+pipeline.stages.[\"Provisioning\"].variables.[\"harness_platform_account_id\"]>"
+      # connector_bcp_github_token      = "<+secrets.getValue(\"connector_bcp_github_token\")>"
+      # connector_bcp_docker_token      = "<+secrets.getValue(\"connector_bcp_docker_token\")>"
+      # connector_bcp_artifactory_token = "<+secrets.getValue(\"connector_bcp_artifactory_token\")>"
+    }
+  })
+  filename = "${path.module}/terraform-pipeline.yml"
+}
+
+data "local_file" "template" {
+  depends_on = [
+    module.bootstrap_connector,
+  ]
+  filename = "../contrib/harness/templates/terraform.yml"
+}
+
+resource "null_resource" "template" {
+  depends_on = [
+    module.bootstrap_connector,
+  ]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    working_dir = path.root
+    command     = <<-EOT
+        curl  -i -X POST '${var.harness_template_endpoint}${local.harness_template_endpoint_account_args}' \
+        --header 'Content-Type: application/yaml' \
+        --header 'x-api-key: ${var.harness_platform_api_key}' -d '
+        ${data.local_file.template.content}
+        '
+        EOT
+  }
+}
+
+# module "bootstrap_harness_delegates" {
+#   depends_on = [
+#     module.bootstrap_harness,
+#   ]
+#   source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=main"
+#   harness_platform_delegates = local.harness_platform_delegates
+#   harness_platform_api_key   = var.harness_platform_api_key
+#   harness_account_id         = var.harness_platform_account_id
+# }
+
+output "details" {
+  value = {
+    organization = module.bootstrap_harness.organization
+    # delegates    = module.bootstrap_harness_delegates.delegates
+    project = module.bootstrap_harness.project
+    # manifests  = module.bootstrap_harness_delegates.manifests
+    connectors = module.bootstrap_connector.connectors
+  }
+}
---
+++ harness-bcp-bootstrap/variables.tf
@@ -0,0 +1,35 @@
+variable "harness_platform_api_key" {
+  sensitive = true
+}
+variable "harness_platform_account_id" {
+  sensitive = true
+}
+variable "harness_connector_bcp_github_token" {
+  sensitive = true
+}
+variable "harness_connector_bcp_docker_token" {
+  sensitive = true
+}
+variable "harness_connector_bcp_artifactory_token" {
+  sensitive = true
+}
+variable "harness_template_endpoint" {
+  default = "https://app.harness.io/gateway/template/api/templates"
+}
+
+locals {
+  harness_platform_projects = tomap({
+    "bcp-lab-org" = var.bcp_lab_org_projects
+  })
+  harness_platform_connectors = merge(
+    local.cristiab_lab_org_connectors
+  )
+  harness_platform_delegates = tomap({
+    k8s = merge(local.cristiab_lab_org_delegates.k8s)
+  })
+  harness_platform_secrets = merge(
+    local.bcp_secrets_org
+  )
+
+  harness_template_endpoint_account_args = "?accountIdentifier=${var.harness_platform_account_id}&orgIdentifier=${module.bootstrap_harness.organization[var.bcp_lab_org_projects.organization_name].org_id}&storeType=INLINE&comments=terraform-generated"
+}
